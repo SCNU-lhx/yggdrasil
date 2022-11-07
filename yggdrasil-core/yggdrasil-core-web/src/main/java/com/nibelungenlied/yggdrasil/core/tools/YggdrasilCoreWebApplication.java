e62bd139f6849a746beef3349dff2f80650dd7bf
lhx <939296853@qq.com>
2022-11-07 20:45:51 +0800

    core web module

---
+++ yggdrasil-core/yggdrasil-core-web/src/main/java/com/nibelungenlied/yggdrasil/core/tools/YggdrasilCoreWebApplication.java
@@ -0,0 +1,13 @@
+package com.nibelungenlied.yggdrasil.core.tools;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class YggdrasilCoreWebApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(YggdrasilCoreWebApplication.class, args);
+    }
+
+}
